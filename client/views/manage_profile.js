6698b2bf00ea8501cc56d4dc442a90753dfd8bcd
Atul Varma <varmaa@gmail.com>
2013-09-30 16:04:56 -0400

    simpler reactive data source logic

--- client/views/manage_profile.js
+++ client/views/manage_profile.js
@@ -1,7 +1,3 @@
-var joinTopics = function(user, name) {
-  return (user.profile[name + 'Topics'] || []).join(', ');
-};
-
 var parseTopics = function(template, name) {
   var selector = "input[name=" + name + "Topics]";
   return template.find(selector).value.split(',').map(function(topic) {
@@ -9,30 +5,29 @@ var parseTopics = function(template, name) {
   });
 };
 
-var topicsChanged = function(template, name) {
-  return joinTopics(template.data, name) !=
-         parseTopics(template, name).join(', ');
-};
-
-var checkFormChanged = function(t) {
-  var changed = topicsChanged(t, 'mentor') || topicsChanged(t, 'noob');
-  Session.set('profileFormChanged', changed);
-};
-
 Template.manageProfile.helpers({
-  mentorTopics: function() { return joinTopics(this, 'mentor'); },
-  noobTopics: function() { return joinTopics(this, 'noob'); },
-  hasFormChanged: function() { return Session.get('profileFormChanged'); }
+  mentorTopics: function() {
+    return (this.profile.mentorTopics || []).join(', ');
+  },
+  noobTopics: function() {
+    return (this.profile.noobTopics || []).join(', ');    
+  },
+  hasFormChanged: function() {
+    if (Session.get('profileForm') === null) return false;
+    return Session.get('profileForm') != JSON.stringify([
+      this.profile.mentorTopics, this.profile.noobTopics
+    ]);
+  }
 });
 
-Template.manageProfile.rendered = function() {
-  checkFormChanged(this);
-};
+Session.set('profileForm', null);
 
 Template.manageProfile.events({
   'keyup input': function(e, t) {
     if (e.keyCode == 13) return t.find('.js-update').click();
-    checkFormChanged(t);
+    Session.set('profileForm', JSON.stringify([
+      parseTopics(t, 'mentor'), parseTopics(t, 'noob')
+    ]));
   },
   'click .js-update': function(e, t) {
     Meteor.users.update({_id: t.data._id}, {
@@ -41,6 +36,5 @@ Template.manageProfile.events({
         'profile.noobTopics': parseTopics(t, 'noob')
       }
     });
-    checkFormChanged(t);
   }
 });
